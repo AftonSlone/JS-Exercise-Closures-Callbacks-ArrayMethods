1fb3df3de047b65e4ece8896aecb026372701c2b
Afton Slone <afton.slone@gmail.com>
2020-10-12 21:17:03 -0400

    Finished All Tasks

--- index.js
+++ index.js
@@ -2,44 +2,43 @@
 
 /**
  * ### Challenge `processFirstItem`
- * 
+ *
  * @instructions
  * Implement a higher-order function called `processFirstItem`.
  * It takes two arguments:
  * @param stringList an array of strings.
  * @param callback function that takes a string as its argument.
  * @returns the result of invoking `callback` with the FIRST element in `stringList`.
- * 
+ *
  * Example of usage of this higher-order function:
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
-*/
+ */
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
 
 // ⭐️ Example Challenge END ⭐️
-
 
 ///// M V P ///////
 
 /* Task 1: `counterMaker`
  * Study the code for counter1 and counter2. Answer the questions below.
- * 
- * 1. What is the difference between counter1 and counter2?
- * 
- * 2. Which of the two uses a closure? How can you tell?
- * 
- * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
-*/
+ * 1. What is the difference between counter1 and counter2?
+ * Counter1 has a function nested inside of it and keeps keeps track of the count to be used again.  counter2 returns count++ but it is being stored outside the function.
+ * 2. Which of the two uses a closure? How can you tell?
+ * counter1 has a closure becasue it is inside of another function
+ * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better?
+ *you would use counter1 if you wanted to keep track of the count and add to it. counter2 would just be for a single use.
+ */
 
 // counter1 code
 function counterMaker() {
   let count = 0;
   return function counter() {
     count++;
-  }
+  };
 }
 
 const counter1 = counterMaker();
@@ -51,16 +50,11 @@ function counter2() {
   return count++;
 }
 
-
 /* Task 2: inning() 
 
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
-}
+const inning = () => Math.floor(Math.random() * 2);
 
 /* Task 3: finalScore()
 
@@ -74,14 +68,22 @@ finalScore(inning, 9) might return:
   "Away": 5,
 }
 
-*/ 
+*/
 
-function finalScore(/*code Here*/){
-
-  /*Code Here*/
-
+function finalScore(inning, numberOfInnings) {
+  let home = 0;
+  let away = 0;
+  for (let i = 0; i < numberOfInnings; i++) {
+    home += inning();
+    away += inning();
+  }
+  return {
+    Home: home,
+    Away: away,
+  };
 }
-
+finalScore(inning, 9);
+// console.log(finalScore(inning, 9));
 /* Task 4: 
 
 Create a function called `scoreboard` that accepts the following parameters: 
@@ -103,9 +105,35 @@ and returns the score at each pont in the game, like so:
 9th inning: awayTeam - homeTeam
 
 Final Score: awayTeam - homeTeam */
+const getInningScore = (inning) => {
+  let home = 0;
+  let away = 0;
+  home += inning();
+  away += inning();
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+  return {
+    Home: home,
+    Away: away,
+  };
+};
+
+function scoreboard(getInningScore, inning, numberOfInnings) {
+  let score = [];
+  for (let i = 0; i < numberOfInnings; i++) {
+    score.push(getInningScore(inning));
+  }
+  let homeTotal = 0;
+  let awayTotal = 0;
+  for (let i = 0; i < score.length; i++) {
+    homeTotal += score[i].Home;
+    awayTotal += score[i].Away;
+  }
+  score.push({
+    "Home Total": homeTotal,
+    "Away Total": awayTotal,
+  });
+  return score;
 }
 
-
+scoreboard(getInningScore, inning, 9);
+console.log(scoreboard(getInningScore, inning, 9));
